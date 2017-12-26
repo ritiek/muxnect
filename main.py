b36947ab1ba09d968db20be5ce93a1820ddeb646
ritiek <ritiekmalhotra123@gmail.com>
2017-12-26 22:47:25 +0530

    I need coffee

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import libtmux
-import flask
+from flask import Flask, request
 import sys
 
 SESSION = 'muxnect'
 
 app = sys.argv[1:]
 parent, *_ = app[0].split()
+print(parent)
 
 server = libtmux.Server()
 session = server.new_session(SESSION)
@@ -14,3 +15,29 @@ session.kill_window('@0')
 pane = window.attached_pane
 
 pane.send_keys(' '.join(app))
+
+
+app = Flask(__name__)
+
+@app.route('/muxnect/<window_name>', methods=['POST'])
+def handle_request(window_name):
+    window = session.find_where({'window_name': window_name})
+
+    if 'enter' in request.form:
+        enter = int(request.form['enter'])
+    else:
+        enter = False
+
+    if 'kill' in request.form:
+        if int(request.form['kill']):
+            window.kill_window()
+
+    pane = window.attached_pane
+    pane.send_keys(request.form['keys'],
+                   enter=enter)
+    return '200'
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', threaded=True, port=6060)
