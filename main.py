c41620da6b82c591c5ead584ad9efc03e685d16e
ritiek <ritiekmalhotra123@gmail.com>
2017-12-28 00:03:22 +0530

    Show help with argparse

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
+from flask import Flask, request
 import libtmux
 from libtmux.exc import *
-from flask import Flask, request
-import sys
-import threading
+
 from distutils import util
+import threading
+import argparse
 
-SESSION = 'muxnect'
 
-app = sys.argv[1:]
-parent, *_ = app[0].split()
-print(parent)
+app = Flask(__name__)
 
-try:
-    server = libtmux.Server()
-    session = server.new_session(SESSION)
-    window = session.new_window(parent)
-    session.kill_window('@0')
-    pane = window.attached_pane
-    pane.send_keys(' '.join(app))
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description='Send input to just about any interactive command-line tool',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-except TmuxSessionExists:
-    pass
+    required = parser.add_argument_group('required arguments')
+
+    required.add_argument(
+        '-c', '--cmd',
+        required=True,
+        type=str,
+        help='interactive command to send input to')
+
+    parser.add_argument(
+        '-d', '--detach',
+        action='store_true',
+        help='detach from ongoing session')
+    parser.add_argument(
+        '-s', '--session-name', default='muxnect',
+        type=str,
+        help='tmux\'s session name')
+    parser.add_argument(
+        '-w', '--window-name', default='mpsyt',
+        type=str,
+        help='tmux\'s window name')
+    parser.add_argument(
+        '-b', '--bind-address', default='127.0.0.1',
+        type=str,
+        help='address to bind on, local network: 0.0.0.0')
+    parser.add_argument(
+        '-p', '--port', default=6060,
+        type=int,
+        help='port number to listen on')
+
+    return parser.parse_args()
 
 
 def query_exists(query, data):
@@ -29,8 +52,6 @@ def query_exists(query, data):
     else:
         return False
 
-
-app = Flask(__name__)
 
 @app.route('/muxnect/<window_name>', methods=['POST'])
 def handle_request(window_name):
@@ -51,10 +72,31 @@ def handle_request(window_name):
 
 
 if __name__ == '__main__':
+    args = get_arguments()
+    session_name = args.session_name
+    window_name = args.window_name
+    detach = args.detach
+    cmd = args.cmd
+
+    print(window_name)
+
+    try:
+        server = libtmux.Server()
+        session = server.new_session(session_name)
+        window = session.new_window(window_name)
+        session.kill_window('@0')
+        pane = window.attached_pane
+        pane.send_keys(cmd)
+
+    except TmuxSessionExists:
+        pass
+
     web_app_args = {'host':'0.0.0.0', 'threaded':True, 'port':6060}
     web_app = threading.Thread(target=app.run, kwargs=web_app_args)
     web_app.start()
-    session.attach_session()
+    if not detach:
+        session.attach_session()
+
     try:
         window.kill_window()
     except LibTmuxException:
